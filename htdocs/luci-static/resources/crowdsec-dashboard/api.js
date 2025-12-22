ac141b757c4a8d798c03e9e7f28caeca7c39bf1d
CyberMind-FR <gandalf@Gk2.net>
2025-12-22 08:40:14 +0100

    Fix ubus

--- htdocs/luci-static/resources/crowdsec-dashboard/api.js
+++ htdocs/luci-static/resources/crowdsec-dashboard/api.js
@@ -2,34 +2,52 @@
 'require baseclass';
 'require rpc';
 
+/**
+ * CrowdSec Dashboard API
+ * Package: luci-app-crowdsec-dashboard
+ * RPCD object: luci.crowdsec-dashboard
+ */
+
 var callStatus = rpc.declare({
-	object: 'luci.crowdsec',
+	object: 'luci.crowdsec-dashboard',
 	method: 'status',
 	expect: { }
 });
 
 var callDecisions = rpc.declare({
-	object: 'luci.crowdsec',
+	object: 'luci.crowdsec-dashboard',
 	method: 'decisions',
 	expect: { decisions: [] }
 });
 
 var callAlerts = rpc.declare({
-	object: 'luci.crowdsec',
+	object: 'luci.crowdsec-dashboard',
 	method: 'alerts',
 	expect: { alerts: [] }
 });
 
 var callBouncers = rpc.declare({
-	object: 'luci.crowdsec',
+	object: 'luci.crowdsec-dashboard',
 	method: 'bouncers',
 	expect: { bouncers: [] }
 });
 
 var callMetrics = rpc.declare({
-	object: 'luci.crowdsec',
+	object: 'luci.crowdsec-dashboard',
 	method: 'metrics',
 	expect: { }
+});
+
+var callMachines = rpc.declare({
+	object: 'luci.crowdsec-dashboard',
+	method: 'machines',
+	expect: { machines: [] }
+});
+
+var callCollections = rpc.declare({
+	object: 'luci.crowdsec-dashboard',
+	method: 'collections',
+	expect: { collections: [] }
 });
 
 return baseclass.extend({
@@ -37,5 +55,7 @@ return baseclass.extend({
 	getDecisions: callDecisions,
 	getAlerts: callAlerts,
 	getBouncers: callBouncers,
-	getMetrics: callMetrics
+	getMetrics: callMetrics,
+	getMachines: callMachines,
+	getCollections: callCollections
 });
